aac897601450a8a39b3d16ccc6860d8a2502b74c
Vasily Ionin <ionin.code@gmail.com>
2021-07-10 13:44:59 +0300

    #13 Add library CMakeLists #12 Add task

---
+++ include/yaclib/task.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <yaclib/container/intrusive_node.hpp>
+
+#include <functional>
+#include <memory>
+
+namespace yaclib {
+
+class ITask : private container::intrusive::detail::Node<ITask> {
+ public:
+  virtual void Call() = 0;
+
+  virtual ~ITask() = default;
+
+  container::intrusive::detail::Node<ITask>* AsNode() noexcept {
+    return static_cast<container::intrusive::detail::Node<ITask>*>(this);
+  }
+};
+
+using ITaskPtr = std::unique_ptr<ITask>;
+
+namespace detail {
+
+template <typename Functor>
+class Task final : public ITask {
+ public:
+  Task(Functor&& f) : _f{std::move(f)} {
+  }
+
+  Task(const Functor& f) : _f{f} {
+  }
+
+ private:
+  void Call() noexcept final {
+    try {
+      _f();
+    } catch (...) {
+      // TODO(MBkkt): create issue
+    }
+  }
+
+  Functor _f;
+};
+
+}  // namespace detail
+
+template <typename Functor>
+ITaskPtr CreateTask(Functor&& f) {
+  using FunctorT = std::remove_reference_t<Functor>;
+  return std::make_unique<detail::Task<FunctorT>>(std::forward<Functor>(f));
+}
+
+}  // namespace yaclib
