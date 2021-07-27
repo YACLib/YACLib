09c84be22557bffb17e86cfbf75cb9b7ad6abe74
MBkkt <MBkkt@users.noreply.github.com>
2021-07-27 18:47:02 +0300

    Add intrusive_ptr

--- include/yaclib/ref.hpp
+++ include/yaclib/ref.hpp
@@ -6,11 +6,11 @@ class IRef {
  public:
   // IExecutor make this before use ITask
   // IThreadFactory make this before use IFunc
-  virtual void Acquire() noexcept = 0;
+  virtual void IncRef() noexcept = 0;
 
   // IExecutor make this after use ITask
   // IThreadFactory make this after use IFunc
-  virtual void Release() noexcept = 0;
+  virtual void DecRef() noexcept = 0;
 
   virtual ~IRef() = default;
 };
