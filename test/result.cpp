94e0fbe5d1a19e258c12c7d627c69c550578d15b
kononovk <kononov.nikolay.nk1@gmail.com>
2021-09-03 02:27:58 +0300

    Add new tests for Result class

--- test/result.cpp
+++ test/result.cpp
@@ -12,7 +12,7 @@ struct NotDefaultConstructible {
   }
 };
 
-TEST(simple, simple) {
+TEST(Simple, Simple) {
   Result<int> result;
   EXPECT_EQ(result.State(), ResultState::Empty);
   result.Set(5);
@@ -20,9 +20,83 @@ TEST(simple, simple) {
   EXPECT_EQ(std::move(result).Ok(), 5);
 }
 
-TEST(simple, not_default_constructible) {
+TEST(Simple, NotDefaultConstructible) {
   Result<NotDefaultConstructible> result;
   result.Set(NotDefaultConstructible{5});
+}
+
+void TestState(ResultState state) {
+  Result<int> result;
+  EXPECT_EQ(result.State(), ResultState::Empty);
+  switch (state) {
+    case ResultState::Value: {
+      std::move(result).Set(1);
+    } break;
+    case ResultState::Error: {
+      std::move(result).Set(std::error_code{});
+    } break;
+    case ResultState::Exception: {
+      std::move(result).Set(std::make_exception_ptr(std::runtime_error{""}));
+    } break;
+    case ResultState::Empty: {
+    } break;
+  }
+  EXPECT_EQ(result.State(), state);
+}
+
+TEST(State, Empty) {
+  TestState(ResultState::Empty);
+}
+
+TEST(State, Error) {
+  TestState(ResultState::Error);
+}
+
+TEST(State, Exception) {
+  TestState(ResultState::Exception);
+}
+
+TEST(State, Value) {
+  TestState(ResultState::Value);
+}
+
+void TestOk(ResultState state) {
+  Result<int> result;
+  EXPECT_EQ(result.State(), ResultState::Empty);
+  switch (state) {
+    case ResultState::Value: {
+      std::move(result).Set(1);
+      EXPECT_NO_THROW(std::move(result).Ok());
+    } break;
+    case ResultState::Error: {
+      std::move(result).Set(std::error_code{});
+      EXPECT_THROW(std::move(result).Ok(), ResultError);
+    } break;
+    case ResultState::Exception: {
+      std::move(result).Set(std::make_exception_ptr(std::runtime_error{""}));
+      EXPECT_THROW(std::move(result).Ok(), std::runtime_error);
+    } break;
+    case ResultState::Empty: {
+      EXPECT_THROW(std::move(result).Ok(), ResultEmpty);
+    } break;
+  }
+  EXPECT_EQ(result.State(), state);
+}
+
+TEST(Ok, Value) {
+  TestOk(ResultState::Value);
+}
+
+TEST(Ok, Error) {
+  TestOk(ResultState::Error);
+}
+
+TEST(Ok, Exception) {
+  TestOk(ResultState::Exception);
+}
+
+TEST(Ok, Empty) {
+  TestOk(ResultState::Empty);
 }
 
 }  // namespace
