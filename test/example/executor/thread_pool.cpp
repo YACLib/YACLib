27682a0e74170af56fdb300ef70ff9b2d8926eb7
kononovk <kononov.nikolay.nk1@gmail.com>
2021-09-13 01:05:56 +0300

    Add examples of usage #16

---
+++ test/example/executor/thread_pool.cpp
@@ -0,0 +1,33 @@
+/**
+ * \example thread_pool.cpp
+ * Simple \ref IThreadPool examples
+ */
+
+#include <yaclib/executor/thread_pool.hpp>
+
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+TEST(Example, ThreadPool) {
+  std::cout << "ThreadPool" << std::endl;
+
+  auto tp = yaclib::executor::MakeThreadPool(4);
+
+  std::atomic<size_t> counter{0};
+
+  static const size_t kIncrements = 100500;
+
+  for (size_t i = 0; i < kIncrements; ++i) {
+    tp->Execute([&counter] {
+      counter.store(counter.load() + 1);
+    });
+  }
+
+  tp->Stop();
+  tp->Wait();
+
+  std::cout << "Counter value = " << counter << ", expected " << kIncrements << std::endl;
+
+  std::cout << std::endl;
+}
