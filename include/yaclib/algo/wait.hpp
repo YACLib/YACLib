6db2fe3e635cf0c8a1958eca158119146635e29a
Vasily Ionin <ionin.code@gmail.com>
2021-10-02 19:46:29 +0300

    Implement ranged Wait/WaitFor/WaitUntil for iterators

--- include/yaclib/algo/wait.hpp
+++ include/yaclib/algo/wait.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <yaclib/algo/detail/wait_impl.hpp>
+#include <yaclib/async/future.hpp>
 
 namespace yaclib {
 
@@ -9,9 +10,31 @@ namespace yaclib {
  *
  * \param fs one or more futures to wait
  */
-template <typename... Futures>
-void Wait(Futures&&... fs) {
+template <typename... T>
+void Wait(Future<T>&... fs) {
   detail::Wait(detail::NoTimeoutTag{}, static_cast<detail::BaseCore&>(*fs.GetCore())...);
+}
+
+/**
+ * Wait until \ref Ready becomes true
+ *
+ * \param begin Iterator to futures to wait
+ * \param end Iterator to futures to wait
+ */
+template <typename Iterator>
+std::enable_if_t<!util::IsFutureV<Iterator>, void> Wait(Iterator begin, Iterator end) {
+  detail::Wait(detail::NoTimeoutTag{}, begin, begin, end);
+}
+
+/**
+ * Wait until \ref Ready becomes true
+ *
+ * \param begin Iterator to futures to wait
+ * \param size count of futures to wait
+ */
+template <typename Iterator>
+void Wait(Iterator begin, size_t size) {
+  detail::Wait(detail::NoTimeoutTag{}, begin, 0, size);
 }
 
 }  // namespace yaclib
