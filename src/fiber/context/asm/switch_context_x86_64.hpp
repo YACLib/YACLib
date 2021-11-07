a8f90bd1e90a46ec53d3c604a90b026ff6c73f5a
Maria Filipanova <myannyax@gmail.com>
2021-11-07 20:19:00 +0300

    [#94] Impl switch context for x86 Linux macOS

---
+++ src/fiber/context/asm/switch_context_x86_64.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#if defined(__APPLE__)
+#define YACLIB_FUNC_NAME(x) _##x
+#else
+#define YACLIB_FUNC_NAME(x) x
+#endif
+
+#define YACLIB_RBX_INDEX 0
+#define YACLIB_RBP_INDEX 1
+#define YACLIB_R12_INDEX 2
+#define YACLIB_R13_INDEX 3
+#define YACLIB_R14_INDEX 4
+#define YACLIB_R15_INDEX 5
+#define YACLIB_RSP_INDEX 6
+#define YACLIB_RIP_INDEX 7
