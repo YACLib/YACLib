059d6717c2b9da53b05fc96334ae563c41cb3d37
Vasily Ionin <ionin.code@gmail.com>
2021-11-14 13:35:34 +0300

    WaitGroup fixes

--- src/algo/wait_group.cpp
+++ src/algo/wait_group.cpp
@@ -2,6 +2,13 @@
 
 namespace yaclib {
 
+template void WaitGroup::Add<void>(Future<void>&);
+
+void WaitGroup::Add(Future<void>&& f) {
+  Add(f);
+  std::move(f).Detach();
+}
+
 void WaitGroup::Wait() {
   _callback.DecRef();
   std::unique_lock guard{_callback.m};
