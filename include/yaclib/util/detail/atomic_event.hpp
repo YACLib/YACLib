d78b87614ffb313deb9df04f438af45e2a720477
Valery Mironov <valera.mironow@gmail.com>
2022-09-11 02:53:57 +0300

    Enable platform wait for more cases: * libstdc++ && have_futex * libc++ && have ulock

--- include/yaclib/util/detail/atomic_event.hpp
+++ include/yaclib/util/detail/atomic_event.hpp
@@ -19,8 +19,13 @@ class /*alignas(kCacheLineSize)*/ AtomicEvent {
 
   void Reset() noexcept;
 
+#if !defined(_LIBCPP_VERSION) || defined(__linux__) || (defined(_AIX) && !defined(__64BIT__))
+  using wait_t = std::int32_t;
+#else
+  using wait_t = std::int64_t;
+#endif
  private:
-  yaclib_std::atomic_int32_t _state = 0;
+  yaclib_std::atomic<wait_t> _state = 0;
 };
 
 }  // namespace yaclib::detail
