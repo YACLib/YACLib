b6064a2fe2667e85e9d9f495dc163a76fc716ed1
harashimahashi <harashimahashi@gmail.com>
2023-04-05 23:57:22 +0300

    shit

--- include/yaclib/coro/channel.hpp
+++ include/yaclib/coro/channel.hpp
@@ -15,6 +15,122 @@
 namespace yaclib {
 
 /*
+ * Bounded MPMC channel
+ * */
+template <typename T>
+class BoundedChannel {
+
+  class PushAwaiter : public detail::Node {
+  public: 
+    PushAwaiter() = default;
+    template<typename... Args>
+    explicit PushAwaiter(BoundedChannel& channel, Args&&... args) : _channel{&channel}, _value{std::forward<Args>(args)...} {}
+
+    bool await_ready() {
+      return _channel != nullptr;
+    }
+
+    template<typename Promise>
+    void await_suspend(yaclib_std::coroutine_handle<Promise> handle) noexcept {
+      _core = &handle.promise();
+      _channel->_senders.PushFront(*this);
+      _channel->_mutex.unlock();
+    }
+
+    void await_resume() {
+    }
+
+    void GetValue(T& value) {
+      value = std::move(_value);
+      _core->_executor->Submit(*_core);
+    }
+  private:
+
+    T _value;
+    detail::BaseCore* _core{nullptr};
+    BoundedChannel* _channel{nullptr};
+  };
+
+  class PopAwaiter : public detail::Node {
+   public:
+    explicit PopAwaiter(BoundedChannel& channel) : _channel{channel} {
+    }
+
+    bool await_ready() {
+      std::unique_lock lock{_channel._mutex};
+      if (!_channel._senders.Empty()) {
+        auto& s = _channel._senders.PopFront();
+        lock.unlock();
+        auto& pa = static_cast<PushAwaiter&>(s);
+        pa.GetValue(_value);
+        return false;
+      }
+      if (_channel._queue.empty()) {
+        lock.release();
+        return false;
+      }
+      _value = std::move(_channel._queue.front());
+      _channel._queue.pop();
+      return true;
+    }
+
+    template <typename Promise>
+    void await_suspend(yaclib_std::coroutine_handle<Promise> handle) noexcept {
+      _core = &handle.promise();
+      _channel._receivers.PushFront(*this);
+      _channel._mutex.unlock();
+    }
+
+    T await_resume() {
+      return std::move(_value);
+    }
+
+    template <typename... Args>
+    void Produce(Args&&... args) {
+      _value = T{std::forward<Args>(args)...};
+      _core->_executor->Submit(*_core);
+    }
+
+   private:
+    detail::BaseCore* _core{};
+    BoundedChannel& _channel;
+    T _value;
+  };
+
+ public:
+  BoundedChannel(size_t size) : _size{size} {}
+
+  template <typename... Args>
+  PushAwaiter Push(Args&&... args) {
+    std::unique_lock lock{_mutex};
+    if (_receivers.Empty()) {
+      if (_queue.size() == _size) {
+        lock.release(); // TODO handle exception in T constructor
+        return PushAwaiter{*this, std::forward<Args>(args)...};
+      }
+      _queue.emplace(std::forward<Args>(args)...);
+    } else {
+      YACLIB_ASSERT(_queue.empty());
+      auto& consumer = _receivers.PopFront();
+      lock.unlock();
+      static_cast<PopAwaiter&>(consumer).Produce(std::forward<Args>(args)...);
+    }
+    return {};
+  }
+
+  PopAwaiter Pop() {
+    return PopAwaiter{*this};
+  }
+
+ private:
+  std::mutex _mutex;
+  std::queue<T> _queue;
+  std::size_t _size;
+  detail::List _senders;
+  detail::List _receivers;
+};
+
+/*
  * Unbounded MPMC channel
  * */
 template <typename T>
--- test/unit/coro/channel.cpp
+++ test/unit/coro/channel.cpp
@@ -44,5 +44,32 @@ TEST(Channel, Simple) {
   tp.Wait();
 }
 
+TEST(Channel, SimpleBounded) {
+  yaclib::FairThreadPool tp{2};
+  yaclib::BoundedChannel<int> ch{25};
+  auto producer = [&]() -> yaclib::Future<> {
+    co_await On(tp);
+    for (int i = 0; i != 100; ++i) {
+      co_await ch.Push(i);
+    }
+    co_return{};
+  };
+  auto consumer = [&]() -> yaclib::Future<> {
+    co_await On(tp);
+    for (int i = 0; i != 100; ++i) {
+      auto r = co_await ch.Pop();
+      std::cout << "on iteration " << i << " received value " << r << std::endl;
+    }
+    co_return{};
+  };
+  auto c = consumer();
+  auto p = producer();
+
+  yaclib::Wait(c, p);
+
+  tp.HardStop();
+  tp.Wait();
+}
+
 }  // namespace
 }  // namespace test
